7def19a9f18cee0f3e1d1dadecdc10e6b267b443
Nikki Apostolakis <nikki.apostolakis@gmail.com>
2018-03-25 18:08:51 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,64 @@
+def sort_array_asc(array)
+  array.sort
+end
+
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+
+  array.sort do |a, b|
+  a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  a = array[1]
+  array[1]=array[2]
+  array[2]=a
+  return array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+
+  array.each do |a|
+    a[2]= "$"
+  end
+  return array
+end
+
+def find_a(array)
+  a_array=[]
+
+  array.each do |a|
+    if(a[0]=="a")
+      a_array << a
+    end
+  end
+  return a_array
+end
+
+def sum_array(array)
+
+  total=0
+  array.each do |a|
+    total = total + a
+  end
+  return total
+end
+
+def add_s(array)
+
+  array.each_with_index do |a, index|
+    if(index!=1)
+      a << "s"
+    end
+  end
+  
+end
 
